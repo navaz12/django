a138a9897f8bfecd25f55211e2a19eff98403159
Navaz Naz <muhammednavazmk@gmail.com>
2020-11-09 10:03:35 +0300

    databse created

--- register/settings.py
+++ register/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'register.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'register',
+        'USER': 'root',
+        'PASSWORD':'',
+        'HOST':'localhost'
+        
     }
 }
 
