4d728027ac9baf504f70318c50fe790974d3af9c
Navaz Naz <muhammednavazmk@gmail.com>
2020-11-09 09:57:02 +0300

    first creat  login views and connect to login url after login connect main project register

--- login/views.py
+++ login/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.urls import path
+from django.http import HttpResponse
 
 # Create your views here.
+def login(request):
+    return HttpResponse("<h1>hello world</h1>")
